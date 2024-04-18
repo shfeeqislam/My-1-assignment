7d74e23b26167b4ec556bb3cc2a3d2f7679b5888
shafeeq <159651588+shfeeqislam@users.noreply.github.com>
2024-04-19 02:55:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let bmi_calculator = await inquirer.prompt([
+    //user input
+    {
+    type:'number',
+    name:"num1", 
+    message:"Enter your first number"
+},
+//select operations
+{
+type:"list",
+choices:["Addition","Subtraction","Multiplication","Division","Reminder"],
+name:'operator',
+message:"Select your operator for Calculation"    
+},
+// second numberinput
+{
+  type:'number',
+  name:"num2", 
+  message:"Enter your second number"
+}
+]);
+
+if(bmi_calculator.operator === "Addition"){
+  console.log(bmi_calculator.num1 + bmi_calculator.num2);
+}else if(bmi_calculator.operator === "Subtraction"){
+  console.log(bmi_calculator.num1 - bmi_calculator.num2);
+}else if(bmi_calculator.operator === "Multiplication"){
+  console.log(bmi_calculator.num1 * bmi_calculator.num2);
+}else if(bmi_calculator.operator === "Division"){
+  console.log(bmi_calculator.num1 / bmi_calculator.num2);
+}else if(bmi_calculator.operator === "Reminder"){
+  console.log(bmi_calculator.num1 % bmi_calculator.num2);
+}else{
+  console.log("invalid operator");
+  
+}
